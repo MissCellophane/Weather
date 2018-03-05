804a687f64b3b9a429a8064ce15d460c3773f6c4
Mlle_z <ZZ@Mlle-zdeMacBook-Air.local>
2018-03-05 22:54:01 +0800

    Static/user

---
+++ static/user.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    function GetRequest() {
+        var url = decodeURI(location.search);
+        if (url.indexOf("?") != -1) {
+            var str = url.substr(1);
+            var key = str.split("=")[0];
+            var value = str.split("=")[1];
+            var request = {};
+            request[key] = value;
+        }
+        return request;
+    }
+
+    request = GetRequest();
+
+    var count = 0;
+    $.post('/apis/forecast_setdefault', {uid: request['ID']}, function(res) {
+        if (res.cities) {
+            $.each(res.cities, function(index, data) {
+                $("#inputbox").append('<input class="city"> <br>');
+                $($(".city")[count]).val(data);
+                count += 1;
+            });
+        }
+    });
+
+    if ($($(".city")[count]).val()=='') {
+        $($(".city")[count]).remove();
+    }
+
+    $("#add").click(function() {
+        $("#inputbox").append('<input class="city"> <br>');
+    });
+
+
+    $("#default").click(function() {
+        var citys = [];
+        $("input.city").each(function() {
+            $(this).val() && citys.push($(this).val());
+        });
+
+        var post_city = JSON.stringify(citys);
+        $.post('/apis/forecast_preference', {pref: post_city, uid: request['ID']}, function(res) {
+            if (res.status) {
+                alert("设置成功");
+            }
+            else {
+                alert("设置失败：请检查城市名称");
+            }
+        });
+    });
+
+
+    $("#search").click(function() {
+        location.href = '/forecast_page?uid=' + request['ID'];
+    });
+
+});
