a4aba3dd7c9671ee8c3d8aaf4bd809de3c8c1872
Mlle_z <ZZ@Mlle-zdeMacBook-Air.local>
2018-03-05 22:49:18 +0800

    Actions

--- action/forecast.py
+++ action/forecast.py
@@ -1,13 +1,76 @@
-from const import hweather_api, LANG, UNIT, KEY
-import requests
+from const import hweather_api, LANG, UNIT, KEY, CONDITIONS
+import requests, json
+from action.login_check import users
 
 
-def forecast_data(LOCATION):
-    parameters = {"location": LOCATION,
-                  "key": KEY,
-                  "lang": LANG,
-                  "unit": UNIT}
-    result = requests.get(hweather_api, params=parameters)
-    data = result.json()["HeWeather6"][0]["daily_forecast"]
-    return data
+def add_to_pref(uid, cities):
+    users.find_one_and_update({'uid': uid}, {"$set": {'ucity': cities}})
+
+
+def preference(uid, cities):
+    print 'cities', cities
+    for city in cities:
+        parameters = {"location": city,
+                      "key": KEY,
+                      "lang": LANG,
+                      "unit": UNIT}
+        r = requests.get(hweather_api, params=parameters)
+        res = r.json()
+        result = res['HeWeather6'][0]
+        print result
+        if result['status'] == 'unknown city':
+            return 0
+
+        add_to_pref(uid, cities)
+
+    return 1
+
+
+def default_cities(uid):
+    result = users.find_one({'uid': uid})
+    if not ('ucity' in result):
+        return []
+    cities = result['ucity']
+    print '----default_cities'
+    print 'cities='
+    for city in cities:
+        print city
+    return cities
+
+
+def forecast_data(uid):
+    print 'uid', uid
+    user = users.find_one({'uid': uid})
+    print 'user'
+    print user, type(user)
+    cities = user['ucity']
+
+    loc = {}
+    for LOCATION in cities:
+        parameters = {"location": LOCATION,
+                      "key": KEY,
+                      "lang": LANG,
+                      "unit": UNIT}
+        r = requests.get(hweather_api, params=parameters)
+        ret = r.json()
+        res = ret['HeWeather6'][0]
+        result = res.get('daily_forecast', {})
+        data = []
+        for day in result:
+            daily = {}
+            for key in CONDITIONS:
+                daily[key] = day[CONDITIONS[key]]
+                # print key, daily[key]
+
+            print json.dumps(daily, indent=4)
+            data.append(daily)
+
+        print '-----forecast_data'
+        print 'data', data
+        loc[LOCATION] =data
+
+    print 'loc'
+    print json.dumps(loc, indent=4)
+
+    return loc
 
